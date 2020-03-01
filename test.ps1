92346cc1168f1de0503b81180390e5ac0fcdcd96
SolderingKnowledge <imaikol@yahoo.com>
2020-02-29 20:02:22 -0800

    forloop

--- test.ps1
+++ test.ps1
@@ -1,2 +1,11 @@
-$array = 1,2,3,4,5
+$myvar = "my var string" # declaring a variable
+
+$array = 1,2,3,4,5 # array
+
 $array | foreach { Write-Host $_ } # 1,2,3,4
+
+for($i=0; $i -lt 5; $i++){
+    Write-Host $i" for loop";
+}
+
+Write-Host $myvar # my var string
